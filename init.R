a1c0e9685a3975fa68400c18b6c4bdaa0faf56d4
acquijanoa <acquijanoa@unc.edu>
2026-05-07 13:31:25 -0400

    Creates SurveyMI package structure

---
+++ init.R
@@ -0,0 +1,13 @@
+#############################
+#
+#   Program: init.R
+#
+#   Author: Alvaro Quijano
+#
+#
+#
+
+## install packages if needed 
+# install.packages(c('devtools','roxygen2'))
+
+devtools::create('SurveyMI')
